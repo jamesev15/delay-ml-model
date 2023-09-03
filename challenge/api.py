27a1e1d9871107de82361cbbcde1595bc19f4bd3
jamesev15 <jamesev1502@gmail.com>
2023-09-02 23:38:30 -0500

    develop api - schemas, exceptions for predict endpoint

--- challenge/api.py
+++ challenge/api.py
@@ -1,13 +1,28 @@
-import fastapi
+import pandas as pd
+from fastapi import FastAPI
 
-app = fastapi.FastAPI()
+from challenge.exceptions import PreprocessException
+from challenge.model import DelayModel
+from challenge.schemas import Payload
+
+app = FastAPI()
+
 
 @app.get("/health", status_code=200)
 async def get_health() -> dict:
-    return {
-        "status": "OK"
-    }
+    return {"status": "OK"}
+
 
 @app.post("/predict", status_code=200)
-async def post_predict() -> dict:
-    return
+async def post_predict(payload: Payload) -> dict:
+    flights_to_process = [flight.data() for flight in payload.flights]
+    flights_df = pd.DataFrame(flights_to_process)
+
+    model = DelayModel()
+    features = model.preprocess(data=flights_df)
+    if isinstance(features, tuple):
+        raise PreprocessException(status_code=400)
+
+    predictions = model.predict(features=features)
+
+    return {"predict": predictions}
